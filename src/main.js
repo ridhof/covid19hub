9786571f8d00e6d0adcb7cd08893ab1babfca495
Ridho Febriansa <officialridhof@gmail.com>
2020-03-27 18:57:06 +0700

    fix by prettier

--- src/main.js
+++ src/main.js
@@ -9,20 +9,20 @@ import vuetify from "./plugins/vuetify";
 Vue.config.productionTip = false;
 
 const requireComponent = require.context(
-  './components',
+  "./components",
   false,
   /Base[A-Z]\w+\.(vue|js)$/
-)
+);
 
 requireComponent.keys().forEach(fileName => {
-  const componentConfig = requireComponent(fileName)
+  const componentConfig = requireComponent(fileName);
 
   const componentName = upperFirst(
-    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
-  )
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, "$1"))
+  );
 
-  Vue.component(componentName, componentConfig.default || componentConfig)
-})
+  Vue.component(componentName, componentConfig.default || componentConfig);
+});
 
 new Vue({
   router,
